f0fb86e04f4a5fe9aabae35d28347092d633c629
blackpan2 <blackpan2@gmail.com>
2018-04-12 10:39:42 -0400

    Project process

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var elementData = {};
-var xhttp = new XMLHttpRequest();
+let elementData = {};
+let xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
         elementData = JSON.parse(this.responseText);
@@ -9,7 +9,13 @@ xhttp.onreadystatechange = function () {
 xhttp.open("GET", "./js/elements.json", true);
 xhttp.send();
 
-var cy;
+let cy;
+let layer;
+let canvas;
+let ctx;
+
+const background = new Image();
+background.src = "maps/usMap1835-export.jpg";
 
 function initCytoscape() {
     cy = cytoscape({
@@ -19,10 +25,9 @@ function initCytoscape() {
             {
                 selector: '.city',
                 style: {
-                    'background-color': 'green',
+                    'background-color': 'grey',
                     width: '10px',
-                    height: '10px',
-                    shape: 'circle'
+                    height: '10px'
                 }
             },
             {
@@ -44,10 +49,19 @@ function initCytoscape() {
                 }
             },
             {
-                selector: '.highlighted',
+                selector: '.canal',
                 style: {
                     'line-color': 'yellow',
-                    'background-color': 'yellow',
+                    'line-style': 'dotted',
+                    'curve-style': 'haystack',
+                    'haystack-radius': 0
+                }
+            },
+            {
+                selector: '.highlighted',
+                style: {
+                    'line-color': 'green',
+                    'background-color': 'green',
                     'transition-property': 'background-color, line-color line-style',
                     'transition-duration': '1s'
                 }
@@ -55,15 +69,14 @@ function initCytoscape() {
         ],
         layout: {name: "preset", fit: false}
     });
-    cy.userZoomingEnabled(false);
-    cy.panningEnabled(false);
     cy.boxSelectionEnabled(false);
     cy.autolock(true);
 
     cy.on('tap', 'node', function (evt) {
+        let eventId = evt.target[0]._private.data.id;
         switch ($("select#usage_method option:checked").val()) {
             case 'distance':
-                var eventNode = cy.$('#' + evt.cyTarget._private.data.id);
+                let eventNode = cy.$('#' + eventId);
                 if (originNode === null) {
                     $('#origin-city-name').text(eventNode.data('name'));
                     originNode = eventNode;
@@ -79,11 +92,11 @@ function initCytoscape() {
                 }
                 break;
             case 'spread':
-                var search = findPath(evt.cyTarget._private.data.id);
-                var i = 0;
-                var highlightSpread = function () {
+                let search = findPath(eventId);
+                let i = 0;
+                let highlightSpread = function () {
                     if (i < search.length) {
-                        var delay = 1000 * fw.distance('#' + evt.cyTarget._private.data.id, search[i]);
+                        const delay = 1000 * fw.distance('#' + eventId, search[i]);
                         if (delay !== Infinity) {
                             setTimeout(function () {
                                 this.addClass('highlighted')
@@ -98,8 +111,41 @@ function initCytoscape() {
         }
     });
 
+    layer = cy.cyCanvas({
+        zIndex: -1
+    });
+    canvas = layer.getCanvas();
+    ctx = canvas.getContext('2d');
+    cy.on("render cyCanvas.resize", function (evt) {
+        layer.resetTransform(ctx);
+        layer.clear(ctx);
+        layer.setTransform(ctx);
+
+        ctx.save();
+        // Draw a background
+        ctx.drawImage(background, 0, 0, 1000, 1260);
+
+        // // Draw text that follows the model
+        // ctx.font = "24px Helvetica";
+        // ctx.fillStyle = "black";
+        // ctx.fillText("This text follows the model", 200, 300);
+
+        // Draw text that is fixed in the canvas
+        layer.resetTransform(ctx);
+        ctx.save();
+        ctx.restore();
+    });
+    layer.resetTransform(ctx);
+    layer.clear(ctx);
+    layer.setTransform(ctx);
+    ctx.save();
+    ctx.drawImage(background, 0, 0, 1000, 1260);
+    layer.resetTransform(ctx);
+    ctx.save();
+    ctx.restore();
 }
-var fw;
+
+let fw;
 spreadToAllNodes = function (originId) {
     fw = cy.elements().floydWarshall(function (edge) {
         return edge.data('length');
@@ -111,24 +157,23 @@ spreadToAllNodes = function (originId) {
 
 findPath = function (originId) {
     return spreadToAllNodes(originId, []);
-    // return cy.elements().bfs(originId, function () {}, false);
 };
 
-var originNode = null;
-var destinationNode = null;
+let originNode = null;
+let destinationNode = null;
 
 getDistance = function () {
-    var travel_method = $("select#travel_method option:checked").val();
+    let travel_method = $("select#travel_method option:checked").val();
     if (travel_method === "") {
         showError("Please select a travel method");
     } else {
-        var collection = cy.collection()
-            .add(cy.$('.city'))
-            .add(cy.$('.' + travel_method));
-        var dijkstra = collection.dijkstra('#' + originNode.data('id'), function () {
-            return this.data('length');
+        let coll = cy.collection()
+            .add(cy.nodes('.city'))
+            .add(cy.edges('.' + travel_method));
+        let dijkstra = coll.dijkstra(originNode, function (edge) {
+            return parseInt(edge._private.data.length);
         }, false);
-        var nodePath = dijkstra.pathTo(destinationNode);
+        let nodePath = dijkstra.pathTo(destinationNode);
         if (nodePath.length === 1) {
             showError("No connection");
         } else {
@@ -139,7 +184,7 @@ getDistance = function () {
 };
 
 highlightNextEle = function (i, nodePath) {
-    var el = nodePath[i];
+    let el = nodePath[i];
     el.addClass('highlighted');
     if (i < nodePath.length - 1) {
         i++;
@@ -148,10 +193,10 @@ highlightNextEle = function (i, nodePath) {
 };
 
 calculateDistance = function (nodePath) {
-    nodePath = nodePath.filter(function (i, ele) {
+    nodePath = nodePath.filter(function (ele, i) {
         return ele.isEdge();
     });
-    var totalDistance = 0;
+    let totalDistance = 0;
     nodePath.forEach(function (ele, i, eles) {
         totalDistance = totalDistance + parseFloat(ele.data('length'));
 
